18b4bbb2387fccfeac6cb5d85f852caa9a5e80c8
Johannes Wachter <johannes.wachter@massiveart.com>
2017-02-25 23:50:57 +0100

    added simple example in default-controller

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,7 +1,7 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
@@ -15,6 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new Pucene\Bundle\DoctrineBundle\PuceneDoctrineBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,25 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $index = $this->get('pucene_doctrine.indices.test');
+
+        $index->index(
+            ['title' => 'Die tolle Farbkombination macht diesen Salat zum absoluten Augenschmaus']
+        );
+        $index->index(
+            ['title' => 'Das könnten die neuen Lieblingsbrownies werden: optisch ein Hingucker und geschmacklich eine Wucht']
+        );
+        $index->index(
+            ['title' => 'Auch die Anhänger der mediterranen Küche sollen in der Kürbiszeit zu ihrem Recht kommen. Hier ein Rezept für eine Tomaten-Kürbis-Suppe']
+        );
+
+        dump($index->search('ein'));
+
+        return $this->render(
+            'default/index.html.twig',
+            [
+                'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
+            ]
+        );
     }
 }
