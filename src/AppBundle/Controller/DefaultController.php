389390bd82e6aa32f190d569054d6f5cd9d6a8b8
Johannes Wachter <johannes.wachter@massiveart.com>
2017-02-26 11:05:32 +0100

    added basic ui to index and search

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,29 +9,25 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="list", methods={"GET"})
+     */
+    public function listAction(Request $request)
+    {
+        $index = $this->get('pucene_doctrine.indices.test');
+
+        return $this->render('default/list.html.twig', ['documents' => $index->search($request->get('q'))]);
+    }
+
+    /**
+     * @Route("/", name="index", methods={"POST"})
      */
     public function indexAction(Request $request)
     {
         $index = $this->get('pucene_doctrine.indices.test');
-
         $index->index(
-            ['title' => 'Die tolle Farbkombination macht diesen Salat zum absoluten Augenschmaus']
-        );
-        $index->index(
-            ['title' => 'Das könnten die neuen Lieblingsbrownies werden: optisch ein Hingucker und geschmacklich eine Wucht']
-        );
-        $index->index(
-            ['title' => 'Auch die Anhänger der mediterranen Küche sollen in der Kürbiszeit zu ihrem Recht kommen. Hier ein Rezept für eine Tomaten-Kürbis-Suppe']
+            ['title' => $request->get('title')]
         );
 
-        dump($index->search('ein'));
-
-        return $this->render(
-            'default/index.html.twig',
-            [
-                'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
-            ]
-        );
+        return $this->redirectToRoute('list');
     }
 }
