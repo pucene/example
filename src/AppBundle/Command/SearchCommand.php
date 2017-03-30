632a6b6c80b3b5fa502358074e94f1073d50ed5e
Johannes Wachter <johannes.wachter@massiveart.com>
2017-03-30 19:47:17 +0200

    refactored example to fit new structure

---
+++ src/AppBundle/Command/SearchCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Pucene\Component\QueryBuilder\Query\FullText\Match;
+use Pucene\Component\QueryBuilder\Search;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SearchCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('app:search')
+            ->addArgument('index')
+            ->addArgument('query');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $client = $this->getContainer()->get('pucene.client');
+        $index = $client->get($input->getArgument('index'));
+
+        $search = new Search(new Match('title', $input->getArgument('query')));
+
+        $start = microtime(true);
+        $result = $index->search($search, 'my_type');
+        $time = microtime(true) - $start;
+
+        $output->writeln('Elapsed time: ' . $time);
+
+        $table = new Table($output);
+
+        $hits = $result['hits'];
+        for ($i = 0, $length = count($hits); $i < $length; ++$i) {
+            $table->addRow(
+                [
+                    $hits[$i]['_id'],
+                    $hits[$i]['_score'],
+                    $hits[$i]['_source']['title'],
+                ]
+            );
+        }
+
+        $table->render();
+    }
+}
